84033f2715b4983b68649185886eb4ee2ae6ef09
Kumaresh <kumareshme2@gmail.com>
2021-01-07 18:59:58 +0530

    model edit

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,8 +5,9 @@ class Student < ApplicationRecord
       has_many :student_courses
       has_many :courses, through: :student_courses
   validates :student_name, presence: true, length: { minimum: 3, maximum: 15 }
-  validates :email_id, presence: true, length: { minimum: 10, maximum: 30 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email_id, presence: true, uniqueness: true, length: { maximum: 35}, format: { with: VALID_EMAIL_REGEX }
   validates :address, presence: true, length: { minimum: 10, maximum: 50 }
   validates :contact_number, presence: true, length: { minimum: 10, maximum: 10 }
-  validates :marks, presence: true, length: { minimum: 2, maximum: 3 }
+  validates :marks, presence: true, :inclusion => 100..500
 end
